b3994046ed0de572f54aee19c4af4a40013315ce
Ali Reza <webdeve1083@gmail.com>
2022-12-04 09:10:47 +0800

    Added all the files...Changed Website name and favicon...Added all the images & SVGs...tailwind setup is ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGreen: "#146B3A",
+        lightGreen: "#00FF6F",
+        yellow: "#F8B229",
+        darkRed: "#BB2528",
+        lightRed: "#EA4630",
+        background: "#212121",
+      },
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "992px" },
+      md: { max: "768px" },
+      sm: { max: "576px" },
+      min2xl: { min: "1535px" },
+    },
   },
   plugins: [],
 };
