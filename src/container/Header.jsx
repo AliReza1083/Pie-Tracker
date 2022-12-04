656b6d4f3368f326d097aef98a814765fc386b9c
Ali Reza <webdeve1083@gmail.com>
2022-12-04 15:38:07 +0800

    Fixed the navbar at the top

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -5,9 +5,11 @@ import CristmasLight from "../assets/cristmas_light.png";
 
 const Header = () => {
   return (
-    <div className="flex justify-center">
-      <div className="relative w-full min2xl:max-w-[1500px] lg:mt-16 grid grid-cols-2 lg:grid-cols-1 items-center px-12 md:px-8 sm:px-4">
+    <div className="flex justify-center py-12 lg:py-[90px]">
+      {/* Main Container */}
+      <div className="relative w-full min2xl:max-w-[1500px] grid grid-cols-2 lg:grid-cols-1 items-center px-12 md:px-8 sm:px-4">
         <BackgroundGradient />
+        {/* item 1 */}
         <div className="flex flex-col items-start gap-4 md:gap-2 lg:items-center">
           <h1 className="relative text-7xl lg:text-[5vw] sm:text-4xl text-lightGreen">
             PIE TRACKER
@@ -24,6 +26,7 @@ const Header = () => {
           <Button path={"/counting"} value="Start Eating" />
         </div>
 
+        {/* item 2 */}
         <div className="flex justify-center p-12">
           <img
             className="w-full max-w-[500px] "
