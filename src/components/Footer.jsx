a635db5e392f67d53b79e53ae76d5ae1d55516ed
Ali Reza <webdeve1083@gmail.com>
2022-12-17 08:59:16 +0800

    added some images for README.md file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,12 @@ const Footer = () => {
   return (
     <div className="text-white w-full bg-darkGreen flex flex-col items-center gap-2 py-4">
       <div className="flex gap-4 text-3xl">
-        <FaDiscord />
-        <AiFillTwitterCircle />
+        <a href="https://discord.gg/ZmAcReCx" target={"_blank"}>
+          <FaDiscord />
+        </a>
+        <a href="https://twitter.com/askimlearning" target={"_blank"}>
+          <AiFillTwitterCircle />
+        </a>
       </div>
       <small>Copyright © Pie 2022</small>
     </div>
